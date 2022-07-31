c20baf8b8d82fc00ba960929236bcf0ae2d6e537
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-07-31 23:00:28 +0700

    Creating First Dart Program

---
+++ Program Dart Pertamamu/first dart program/bin/first_dart_program.dart
@@ -0,0 +1,5 @@
+import 'package:first_dart_program/first_dart_program.dart' as first_dart_program;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_dart_program.calculate()}!');
+}
---
+++ Program Dart Pertamamu/first dart program/test/first_dart_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_dart_program/first_dart_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
