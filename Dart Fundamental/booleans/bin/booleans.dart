df97554c17346db3f64c0f8b96b8835e99cf28b5
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-08-01 00:08:01 +0700

    Booleans
    Learn boolean bang on dart

---
+++ Dart Fundamental/booleans/bin/booleans.dart
@@ -0,0 +1,15 @@
+import 'package:booleans/booleans.dart' as booleans;
+
+void main(List<String> arguments) {
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+  // note: ! = not or bang
+
+  if(true) {
+    print("It's true");
+  } else {
+    print("It's False");
+  }
+}
