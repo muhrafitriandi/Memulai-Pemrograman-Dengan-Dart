8ef231bff099b60c250b6f4a42e7a6b9169f64c6
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-08-01 03:01:23 +0700

    For Loops

---
+++ Control Flow/for loops/bin/for_loops.dart
@@ -0,0 +1,15 @@
+import 'package:for_loops/for_loops.dart' as for_loops;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // for (int i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j <= i; j++) {
+      stdout.write('*');
+    }
+    stdout.writeln();
+  }
+}
