f56f36b62397a8519f8a85ec2e5b1daefefd11a5
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-07-31 23:55:56 +0700

    Numbers
    Learn int and double on dart

---
+++ Dart Fundamental/numbers/bin/numbers.dart
@@ -0,0 +1,25 @@
+import 'package:numbers/numbers.dart' as numbers;
+
+void main(List<String> arguments) {
+  // int
+  var number = 2020;
+  var hex = 0xDEADBEEF;
+
+  // double
+  var decimal = 1.2;
+  var pi = 3.14;
+
+  double withoutDecimal = 7; // Sama dengan double withoutDecimal = 7.0
+
+  // String -> int
+  var eleven = int.parse('11');
+
+  // String -> double
+  var elevenPointTwo = double.parse('11.2');
+
+  // int -> String
+  var elevenAsString = 11.toString();
+
+  // double -> String
+  var piAsString = 3.14159.toStringAsFixed(2); // String piAsString = '3.14'
+}
