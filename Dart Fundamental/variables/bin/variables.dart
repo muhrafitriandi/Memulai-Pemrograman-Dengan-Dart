e3b5192a5076c31fe9daa79abc324c5013295d79
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-07-31 23:19:37 +0700

    Variables
    Learn variables on dart

---
+++ Dart Fundamental/variables/bin/variables.dart
@@ -0,0 +1,13 @@
+import 'package:variables/variables.dart' as variables;
+
+void main(List<String> arguments) {
+  var greetings = 'Hello Dart!';
+  print(greetings);
+
+  // var myAge = 20;
+  // print(myAge);
+
+  var myAge;
+  // myAge = 20;
+  print(myAge);
+}
