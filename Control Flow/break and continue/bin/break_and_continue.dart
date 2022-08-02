14a1fd7bbb6400031f50888973a81f7ea0921c2d
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-08-03 06:23:43 +0700

    Break and Continue
    Learn search prime number, and display number 1-10 except multiples of three

---
+++ Control Flow/break and continue/bin/break_and_continue.dart
@@ -0,0 +1,26 @@
+import 'package:break_and_continue/break_and_continue.dart' as break_and_continue;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // program mencari bilangan prima
+  // 20 bilangan prima pertama
+  var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71];
+  stdout.write('Masukkan bilangan prima : ');
+  var searchNumber = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < primeNumbers.length; i++) {
+    if (searchNumber == primeNumbers[i]) {
+      print('$searchNumber adalah bilangan prima ke-${i+1}');
+      break;
+    }
+    print('$searchNumber != ${primeNumbers[i]}');
+  }
+
+  // program menampilkan angka 1-10 kecuali kelipatan tiga
+  for (int i = 1; i <= 10; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
---
+++ Control Flow/break and continue/test/break_and_continue_test.dart
@@ -0,0 +1,8 @@
+import 'package:break_and_continue/break_and_continue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
